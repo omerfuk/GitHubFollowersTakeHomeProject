9cdd715aaa32c03f84bd504dc11109e9bd0d8415
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-06-18 14:31:51 +0300

    planning network calls, created network calls created enums, old type of enum calling(on the next commit it will be changed to result type enum)

---
+++ GitHubFollowersTakeHomeProject/Managers/NetworkManager.swift
@@ -0,0 +1,64 @@
+//
+//  NetworkManager.swift
+//  GitHubFollowersTakeHomeProject
+//
+//  Created by Ömer Faruk Kılıçaslan on 18.06.2022.
+//
+
+import Foundation
+
+
+class NetworkManager {
+    
+    static let shared   = NetworkManager()
+    
+    let baseURL         = "https://api.github.com/users/"
+    
+    private init() {}
+    
+    
+    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, ErrorMessage?) -> Void) {
+        
+        let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
+        
+        guard let url = URL(string: endpoint) else {
+            completed(nil, .invalidUsername)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let _ = error {
+                completed(nil, .unableToComplete)
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(nil, .invalidResponse)
+                return
+            }
+            
+            
+            guard let data = data else {
+                completed(nil, .invalidData)
+                return
+            }
+            
+            //all the cases handled. its time to start parsing json
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let followers = try decoder.decode([Follower].self, from: data)
+                completed(followers, nil)
+                
+            } catch  {
+                completed(nil, .invalidData)
+                
+            }
+            
+        }
+        
+        task.resume()
+    }
+}
---
+++ GitHubFollowersTakeHomeProject/Utilities/ErrorMessage.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorMessage.swift
+//  GitHubFollowersTakeHomeProject
+//
+//  Created by Ömer Faruk Kılıçaslan on 18.06.2022.
+//
+
+import Foundation
+
+
+enum ErrorMessage: String {
+    
+    case invalidUsername = "This username created an invalid request, please try again later"
+    case unableToComplete = "Unable to complete your request, please check your internet connection"
+    case invalidResponse = "Invalid response from the server. Please try again..."
+    case invalidData = "The data received from the server was invalid. Please try again..."
+    
+}
